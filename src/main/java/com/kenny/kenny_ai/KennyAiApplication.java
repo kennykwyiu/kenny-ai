bbfed5e957482db751313d59ce3d1238b1437777
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-12 14:32:23 +0800

    init commit

---
+++ src/main/java/com/kenny/kenny_ai/KennyAiApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.kenny_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KennyAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KennyAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenny/kenny_ai/KennyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.kenny_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KennyAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
