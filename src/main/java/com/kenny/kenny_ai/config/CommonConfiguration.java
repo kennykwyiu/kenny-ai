19922dcc007c8dc683e8a38c633086a04e282d35
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-12 17:12:27 +0800

    add defaultAdvisors

--- src/main/java/com/kenny/kenny_ai/config/CommonConfiguration.java
+++ src/main/java/com/kenny/kenny_ai/config/CommonConfiguration.java
@@ -1,17 +1,20 @@
 package com.kenny.kenny_ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfiguration {
+    
     @Bean
     public ChatClient chatClient(OllamaChatModel model) {
         return ChatClient
                 .builder(model)
                 .defaultSystem("you are a senior and helpful software engineer, and your name is Jarvis")
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
     }
 }
