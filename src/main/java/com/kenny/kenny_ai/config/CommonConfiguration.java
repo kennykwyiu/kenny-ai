6ac1f02d2fe84b3740c36a7aa84d825c124f5e4b
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-13 21:33:51 +0800

    add ChatMemory

--- src/main/java/com/kenny/kenny_ai/config/CommonConfiguration.java
+++ src/main/java/com/kenny/kenny_ai/config/CommonConfiguration.java
@@ -1,20 +1,36 @@
 package com.kenny.kenny_ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfiguration {
-    
+
     @Bean
-    public ChatClient chatClient(OllamaChatModel model) {
+    public ChatMemory chatMemory(ChatMemoryRepository repo) {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(repo)
+                .maxMessages(20) // adjust as needed
+                .build();
+    }
+
+    @Bean
+    public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory) {
         return ChatClient
                 .builder(model)
                 .defaultSystem("you are a senior and helpful software engineer, and your name is Jarvis")
-                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new SimpleLoggerAdvisor(),
+                                    MessageChatMemoryAdvisor
+                                            .builder(chatMemory)
+                                            .build()
+                )
                 .build();
     }
 }
