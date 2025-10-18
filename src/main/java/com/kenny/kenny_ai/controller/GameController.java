5d9139bfdf687ee520e7786a9b14f6a00fe120fe
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-19 01:53:56 +0800

    GameController

---
+++ src/main/java/com/kenny/kenny_ai/controller/GameController.java
@@ -0,0 +1,14 @@
+package com.kenny.kenny_ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
+@RestController
+@RequestMapping("/ai")
+public class GameController {
+
+}
