11394b81a2e4f718c689ec716ae95c8f5cf8cb9a
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-20 22:06:42 +0800

    implement GameController

--- src/main/java/com/kenny/kenny_ai/controller/GameController.java
+++ src/main/java/com/kenny/kenny_ai/controller/GameController.java
@@ -11,4 +11,19 @@ import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 @RequestMapping("/ai")
 public class GameController {
 
+    private final ChatClient gameChatClient;
+
+    public GameController(ChatClient gameChatClient) {
+        this.gameChatClient = gameChatClient;
+    }
+
+    @RequestMapping(value = "/game", produces = "text/html;charset=utf-8")
+    public Flux<String> chat(String prompt, String chatId) {
+
+        return gameChatClient.prompt()
+                .user(prompt)
+                .advisors(a -> a.param(CONVERSATION_ID, chatId))
+                .stream()
+                .content();
+    }
 }
