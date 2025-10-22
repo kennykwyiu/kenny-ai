71bb02ab1d93943e12c5c3999c9f52b958acc59f
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-22 21:57:05 +0800

    add SchoolMapper

---
+++ src/main/java/com/kenny/kenny_ai/mapper/SchoolMapper.java
@@ -0,0 +1,7 @@
+package com.kenny.kenny_ai.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kenny.kenny_ai.entity.po.School;
+
+public interface SchoolMapper extends BaseMapper<School> {
+}
