6aae5065a037609254d2e42bb5c710a8e0ffa7c2
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-22 21:57:14 +0800

    add CourseReservationMapper

---
+++ src/main/java/com/kenny/kenny_ai/mapper/CourseReservationMapper.java
@@ -0,0 +1,7 @@
+package com.kenny.kenny_ai.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kenny.kenny_ai.entity.po.CourseReservation;
+
+public interface CourseReservationMapper extends BaseMapper<CourseReservation> {
+}
