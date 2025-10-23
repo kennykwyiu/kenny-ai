64d057650c75293e202cf3c2415f8e22d1863727
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-23 21:47:49 +0800

    add ICourseReservationService

---
+++ src/main/java/com/kenny/kenny_ai/service/ICourseReservationService.java
@@ -0,0 +1,7 @@
+package com.kenny.kenny_ai.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.kenny.kenny_ai.entity.po.CourseReservation;
+
+public interface ICourseReservationService extends IService<CourseReservation> {
+}
