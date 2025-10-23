14607de9aaf6421ae09ae5b270bc975a0e80bb82
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-23 21:48:14 +0800

    add ISchoolService

---
+++ src/main/java/com/kenny/kenny_ai/service/ISchoolService.java
@@ -0,0 +1,7 @@
+package com.kenny.kenny_ai.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.kenny.kenny_ai.entity.po.School;
+
+public interface ISchoolService extends IService<School> {
+}
