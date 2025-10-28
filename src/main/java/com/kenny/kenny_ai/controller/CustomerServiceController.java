3ab6cc4c0a06af446329390e1a89fc06982ef0e1
kennyyiu <kenny.kwyiu@gmail.com>
2025-10-28 22:23:23 +0800

    added CustomerServiceController

---
+++ src/main/java/com/kenny/kenny_ai/controller/CustomerServiceController.java
@@ -0,0 +1,12 @@
+package com.kenny.kenny_ai.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ai")
+public class CustomerServiceController {
+
+}
