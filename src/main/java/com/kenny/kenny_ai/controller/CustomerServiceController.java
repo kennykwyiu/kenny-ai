3108b9f61ab3f56bcf12c75a7da13aff873ac7ed
kennyyiu <kenny.kwyiu@gmail.com>
2025-11-03 21:10:00 +0800

    add ai/service api

--- src/main/java/com/kenny/kenny_ai/controller/CustomerServiceController.java
+++ src/main/java/com/kenny/kenny_ai/controller/CustomerServiceController.java
@@ -1,12 +1,33 @@
 package com.kenny.kenny_ai.controller;
 
+import com.kenny.kenny_ai.repository.ChatHistoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/ai")
 public class CustomerServiceController {
 
+    private final ChatClient serviceChatClient;
+
+    private final ChatHistoryRepository chatHistoryRepository;
+
+    @RequestMapping(value = "/service", produces = "text/html;charset=utf-8")
+    public Flux<String> service(String prompt, String chatId) {
+
+        chatHistoryRepository.save("service", chatId);
+
+        return serviceChatClient.prompt()
+                .user(prompt)
+//                .advisors(new org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor())
+                .advisors(a -> a.param(CONVERSATION_ID, chatId))
+                .stream()
+                .content();
+    }
 }
