d48b35a1e6b694ee2ae80497082e2a25595a9848
kennyyiu <kenny.kwyiu@gmail.com>
2025-11-14 23:42:19 +0800

    add EmbeddingSimilarityUtils

---
+++ src/main/java/com/kenny/kenny_ai/utils/EmbeddingSimilarityUtils.java
@@ -0,0 +1,12 @@
+package com.kenny.kenny_ai.utils;
+
+import java.util.Locale;
+
+/**
+ * Utilities for computing similarity/distance on text embeddings.
+ * Model-agnostic: works with nomic-embed-text, text-embedding-3, etc.
+ */
+public final class EmbeddingSimilarityUtils {
+
+    private EmbeddingSimilarityUtils() {}
+}
