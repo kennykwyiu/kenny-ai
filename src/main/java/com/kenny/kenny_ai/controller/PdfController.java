735a96997a880198300c1e310b7fce709029e951
kennyyiu <kenny.kwyiu@gmail.com>
2025-11-16 14:55:05 +0800

    add annotation in PdfController

--- src/main/java/com/kenny/kenny_ai/controller/PdfController.java
+++ src/main/java/com/kenny/kenny_ai/controller/PdfController.java
@@ -1,4 +1,13 @@
 package com.kenny.kenny_ai.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ai/pdf")
 public class PdfController {
 }
