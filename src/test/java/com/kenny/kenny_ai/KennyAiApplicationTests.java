09994e4c4999de569fb8b4b824b9260534216a6c
kennyyiu <kenny.kwyiu@gmail.com>
2025-11-27 21:32:53 +0800

    add OllamaEmbeddingModel

--- src/test/java/com/kenny/kenny_ai/KennyAiApplicationTests.java
+++ src/test/java/com/kenny/kenny_ai/KennyAiApplicationTests.java
@@ -1,10 +1,15 @@
 package com.kenny.kenny_ai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KennyAiApplicationTests {
+
+	@Autowired
+	private OllamaEmbeddingModel embeddingModel;
 
 	@Test
 	void contextLoads() {
